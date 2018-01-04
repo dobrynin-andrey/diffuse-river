79ff38fdbcdd87d59e24af6bc18e5e7cc533d2b7
Andrey Dobrynin <andrdobrynin@gmail.com>
2018-01-05 01:33:07 +0700

    Changed sidebar-menu

--- web/js/custom.js
+++ web/js/custom.js
@@ -9,7 +9,7 @@
 
      $(function () {
          $(".preloader").fadeOut();
-         $('#side-menu').metisMenu();
+         $('#side-menu').metisMenu(); 
      });
 
      /* ===== Theme Settings ===== */
@@ -55,14 +55,14 @@
                  if (height > topOffset) {
                      $("#page-wrapper").css("min-height", (height) + "px");
                  }
-             },
-             url = window.location,
+             }// Отлючил не отрабатывет, так как нужно
+             /*url = window.location,
              element = $('ul.nav a').filter(function () {
                  return this.href === url || url.href.indexOf(this.href) === 0;
-             }).addClass('active').parent().parent().addClass('in').parent();
-         if (element.is('li')) {
+             }).addClass('active').parent().parent().addClass('in').parent();*/
+         /*if (element.is('li')) {
              element.addClass('active');
-         }
+         }*/
          $(window).ready(set);
          $(window).bind("resize", set);
      });
