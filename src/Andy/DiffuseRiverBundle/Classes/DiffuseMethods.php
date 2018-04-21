8e5af8bf4ae0a468632442c0ad8683ec86071ac0
Andrey Dobrynin <andrdobrynin@gmail.com>
2018-04-22 00:02:11 +0700

    Added DiffuseMethods and update calculate algoritm

---
+++ src/Andy/DiffuseRiverBundle/Classes/DiffuseMethods.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrey D.
+ * Date: 19.04.18
+ * Time: 15:00
+ */
+
+namespace Andy\DiffuseRiverBundle\Classes;
+
+
+class DiffuseMethods
+{
+    const typeMethods = array('A', 'B');
+
+
+
+}
--- src/Andy/DiffuseRiverBundle/Controller/CalculateController.php
+++ src/Andy/DiffuseRiverBundle/Controller/CalculateController.php
@@ -2,173 +2,191 @@
 
 namespace Andy\DiffuseRiverBundle\Controller;
 
+use Andy\DiffuseRiverBundle\Classes\DiffuseMethods;
 use Andy\DiffuseRiverBundle\Entity\Parameter;
 use Andy\DiffuseRiverBundle\Entity\Point;
 use Andy\DiffuseRiverBundle\Entity\Project;
 use Andy\DiffuseRiverBundle\Controller\PointController;
 
+use Andy\DiffuseRiverBundle\Entity\Result;
+use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class CalculateController
+ * @package Andy\DiffuseRiverBundle\Controller
+ */
 
 class CalculateController extends Controller
 {
     public function indexAction(Request $request)
     {
-        $result['success'] = array();
-        $result['error'] = array();
-       /* $data = array(
-            'start' => '4',
-            'end'   => '4'
-        );*/
-        //$data = '{"start":3,"end":4}';
-        if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
-            $data = json_decode($request->getContent(), true);
-           // $data = json_decode($data, true);
-            $request->request->replace(is_array($data) ? $data : array());
-        }
 
-        try {
+        if ($request->isMethod('post')) {
 
-
-            if (empty($data))
-                throw new \Exception('Не выбраны точки расчета!');
-            if (empty($data['start']))
-                throw new \Exception('Не выбрана начальная точка расчета!');
-            if (empty($data['end']))
-                throw new \Exception('Не выбрана конечная точка расчета!');
-
-
+            $method = $request->get('method');
 
             $em = $this->getDoctrine()->getManager();
-            // Делаем запорос в БД, на входе: точки из запроса
-            $points = $em->getRepository('AndyDiffuseRiverBundle:Point')->findBy(array(
-                'id' =>  $data
-            ));
 
-            if (empty($points[0]))
-                throw new \Exception("Точка с id: " . $data['start'] ." не существет в базе!");
-            else
-                $startPoint = $points[0];
+            $project = $point = $parameter = '';
 
-            if (empty($points[1]))
-                throw new \Exception("Точка с id: " . $data['end'] ." не существет в базе!");
-            else
-                $endPoint = $points[1];
+            if (!empty($request->get('project'))) {
+                $project = $em->getRepository('AndyDiffuseRiverBundle:Project')
+                    ->find($request->get('project'));
+            }
 
-            if (!$paramStartPoint = $this->getValueParameter($startPoint))
-                throw new \Exception("Данные для точки с id: " . $data['end'] ." не существуют в базе!");
-            if (!$paramEndPoint = $this->getValueParameter($endPoint))
-                throw new \Exception("Данные для точки с id " . $data['end'] ." не существуют в базе!");
+            if (!empty($request->get('point'))) {
+                $point = $em->getRepository('AndyDiffuseRiverBundle:Point')
+                    ->find($request->get('point'));
+            }
+
+            if (!empty($request->get('parameter'))) {
+                $parameter = $em->getRepository('AndyDiffuseRiverBundle:Parameter')
+                    ->find($request->get('parameter'));
+            }
+
+            try {
+
+                if (empty($method) || $method == '0')
+                    throw new Exception('Не выбран алгоритм расчета!');
+
+                if (!in_array($method, DiffuseMethods::typeMethods))
+                    throw new Exception('Выбранный метод отсутствует!');
+
+                // Выводим значения параметров текущей точки
+                $arParameter = $em->getRepository('AndyDiffuseRiverBundle:ParamValue')
+                    ->getValueFromPointAndParameter($point, $parameter);
+
+                // Получаем значения Расхода поды (Q)
+                $arParamQ = $em->getRepository('AndyDiffuseRiverBundle:ParamValue')
+                    ->getValueByCodeParameter($point, 'Q');
 
 
+                switch ($method) {
+                    case 'A':
+
+                        /**
+                         *  Вычисление L0 и Kd по формулам Случай A
+                         */
+
+                        // Вводим переменные для вычислений
+                        $sumXmultiplyK = 0;
+                        $sumX = 0;
+                        $sumK = 0;
+                        $sumSquareX = 0;
+                        $n = 0;
 
 
-            $result['success'] = [1];
+                        // находим m (количество элементов)
+                        $mParameter = count($arParameter);
 
-        } catch (\Exception $e) {
-            $result['error'][] = $e->getMessage();
-            return $this->json(json_encode($result, JSON_UNESCAPED_UNICODE));
+                        // Перебираем полученные параметра
+                        foreach ($arParameter as $keyParameter => $itemParameter) {
+                            // Перебираем данные параметра Q
+                            foreach ($arParamQ as $keyParamQ => $itemParamQ) {
+                                // Если даты совпадают, вычисляем, иначе выводим ошибку
+                                if ($itemParameter['date'] == $itemParamQ['date']) {
+
+                                    /*
+                                     *  X = 1/Q
+                                     */
+
+                                    if (isset($X))
+                                        unset($X);
+
+                                    if (isset($K))
+                                        unset($K);
+
+
+                                    if (current($itemParameter)->getValue() == -1) {
+                                        $n++;
+                                        continue;
+                                    } else {
+                                        $K = (double)current($itemParameter)->getValue();
+                                        $X = (double)1/$itemParamQ['value'];
+                                    }
+
+                                    $sumXmultiplyK +=  $X * $K;
+
+                                    $sumX += $X;
+
+                                    $sumK += $K;
+
+                                    $sumSquareX += $X * $X;
+
+                                }
+                            }
+                        }
+
+                        if (isset($L0))
+                            unset($L0);
+
+                        $L0 =
+                            ($mParameter * $sumXmultiplyK - $sumX * $sumK)
+                                /
+                            ($mParameter * $sumSquareX - $sumX * $sumX);
+
+
+                        if (isset($Kd))
+                            unset($Kd);
+
+                        $Kd = 1 / $mParameter * ($sumK - $L0 * $sumX);
+
+                        $result = new Result();
+
+                        $result->setProjectId($project->getId());
+                        $result->setPointId($point->getId());
+                        $result->setParameterId($parameter->getId());
+
+                        if (isset($resultValue))
+                            unset($resultValue);
+
+                        $resultValue = json_encode(array(
+                            'Kd' => $Kd,
+                            //'L0' => $L0 Пока оставим только Kd
+                        ));
+
+
+                        if (!empty($resultValue))
+                            $result->setValue($resultValue);
+
+                        $em->persist($result); // "Коммитим" изменения перед отпоавкой в БД
+                        $em->flush();
+
+                        $this->addFlash('success', "Данные случая $method успешно рассчитаны!");
+                        $this->addFlash('error', "Параметры с данными '-1', не участвуют в рассчетах, таких данных: $n");
+                        return $this->redirectToRoute('parameter_value_show', array(
+                            'project' => $project->getId(),
+                            'id' => $point->getId(),
+                            'parameter' => $parameter->getId(),
+                        ));
+
+                        break;
+                }
+
+                $this->addFlash('error', "Внимание! Случай $method, на данный момент не имеет алгоритма расчета!");
+                return $this->redirectToRoute('parameter_value_show', array(
+                    'project' => $project->getId(),
+                    'id' => $point->getId(),
+                    'parameter' => $parameter->getId(),
+                ));
+
+            } catch (Exception $e) {
+                $this->addFlash('error', $e->getMessage());
+                return $this->redirectToRoute('parameter_value_show', array(
+                    'project' => $project->getId(),
+                    'id' => $point->getId(),
+                    'parameter' => $parameter->getId(),
+                ));
+            }
+
+        } else {
+            $this->addFlash('error', 'Форма не отправлена, повторите еще раз!');
+            return $this->redirectToRoute('andy_diffuse_river_homepage');
         }
 
-
-
-//        foreach ($paramStartPoint as $key => $itemParamStartPoint) {
-//
-//        }
-//
-//        die();
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//        if (count($paramStartPoint) != count($paramEndPoint)) {
-//            dump('error');
-//            dump($paramStartPoint);
-//            dump(count($paramStartPoint));
-//            dump(count($paramEndPoint));
-//            die();
-//        }
-//
-//        $arResult = array();
-//        foreach ($paramStartPoint as $key => $itemParamStartPoint) {
-//
-//            // Получем значения точек
-//            if (!empty($itemParamStartPoint['value']) && $itemParamStartPoint['value'] != 0) {
-//                $startVal = (float)$itemParamStartPoint['value'];
-//            } else {
-//                $startVal = (float)-1;
-//            }
-//
-//            if (!empty($paramEndPoint[$key]['value']) && $paramEndPoint[$key]['value'] != 0) {
-//                $endVal = (float)$paramEndPoint[$key]['value'];
-//            } else {
-//                $endVal = (float)-1;
-//            }
-//
-//
-//
-//            $arResult[] = $startVal / $endVal;
-//        }
-//        dump($arResult);
-//
-//        //die();
-//        //dump($paramStartPoint);
-//        echo '<pre>';
-//        dump($arResult);
-//        dump($points);
-//        dump($paramStartPoint);
-//        dump($paramEndPoint);
-//        echo '</pre>';
-//
-//
-//        die();
-//        //$paramEndPoint = $this->renderParameters($endPoint);
-//
-//       /* foreach ($paramStartPoint as $k => $itemParam) {
-//            $arItemParam = $this->getValueParameter($itemParam);
-//
-//            dump($itemParam);
-//            dump($arItemParam);
-//            die();
-//        }*/
-//
-//        dump($points);
-//        dump($paramStartPoint);
-//        dump($paramEndPoint);
-//        die($data);
-        return $this->json(json_encode($result, JSON_UNESCAPED_UNICODE));
-    }
-
-    /**
-     * @param Point $point
-     * @return mixed
-     */
-
-    public function getValueParameter(Point $point)
-    {
-        $em = $this->getDoctrine()->getManager();
-
-
-        // Запрос объединяет ParamValue и ParamDate, выводит только массив parameterId текущей точки
-        $query = $em->createQuery(
-            'SELECT pv.id, pv.parameterId, pv.value, pd.date
-                FROM AndyDiffuseRiverBundle:ParamValue pv
-                JOIN AndyDiffuseRiverBundle:ParamDate pd
-                WHERE pv.paramDateId = pd.id 
-                AND pd.pointId = :point
-                ORDER BY pv.parameterId'
-        )->setParameter('point', $point);
-
-
-        return $query->getResult();
     }
 
 }
